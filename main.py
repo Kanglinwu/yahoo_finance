07fe0c7bae9b6967119af97200728ad393ebd5eb
Gary Wu (YT-OPS) <09060.gary.wu@ICT888.NET>
2020-07-06 00:00:58 +0800

    20200706 version 1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
+from yahooquery import Ticker
+
+stocks = Ticker('aapl')
+
+# print(aapl.asset_profile) ## return company dict format
+temp_stock = stocks.asset_profile
+# print(temp_stock)
+# print(type(temp_stock))
+
+def returnDict(target):
+
+
+stock_asset_profile = lambda a: a['aapl']
+
+
+# for x in stocks[0]:
+#     print(x, stocks[x])
+#     print('#########')
+
+print(stock_asset_profile(temp_stock))
+print(type(stock_asset_profile(temp_stock)))
+# for x in stock_asset_profile:
+#     print(x,stock_asset_profile[x])
+#     print('#########')
