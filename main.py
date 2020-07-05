422c222584b5bb2f7b7f76274ea04c1737d78876
Kisl <kislxhotmail@gmail.com>
2020-07-06 01:06:42 +0800

    Plus esg_scores

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from yahooquery import Ticker
 
 stocks = Ticker('aapl')
 
+## Refer - https://github.com/dpguthrie/yahooquery
+
 # print(aapl.asset_profile) ## return company dict format
-temp_stock = stocks.asset_profile
+stock_asset_profile = stocks.asset_profile
+stock_esg_scores = stocks.esg_scores
 # print(temp_stock)
 # print(type(temp_stock))
 
-def returnDict(target):
-
-
-stock_asset_profile = lambda a: a['aapl']
-
+returndict = lambda a: a['aapl']
 
 # for x in stocks[0]:
 #     print(x, stocks[x])
 #     print('#########')
 
-print(stock_asset_profile(temp_stock))
-print(type(stock_asset_profile(temp_stock)))
+print('Asset profile start')
+print(returndict(stock_asset_profile))
+print('#########')
 # for x in stock_asset_profile:
 #     print(x,stock_asset_profile[x])
 #     print('#########')
+
+print('Esg Scores start')
+print(returndict(stock_esg_scores))
+print('#########')
