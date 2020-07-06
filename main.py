fa2ddcd723a230daa236c262716560daeb32e721
Gary Wu (YT-OPS) <09060.gary.wu@ICT888.NET>
2020-07-06 16:26:58 +0800

    find the method to query the api

--- main.py
+++ main.py
@@ -5,25 +5,33 @@ stocks = Ticker('aapl')
 
 ## Refer - https://github.com/dpguthrie/yahooquery
 
-# print(aapl.asset_profile) ## return company dict format
-stock_asset_profile = stocks.asset_profile
-stock_esg_scores = stocks.esg_scores
-# print(temp_stock)
-# print(type(temp_stock))
+# stock_asset_profile = stocks.asset_profile
+# stock_esg_scores = stocks.esg_scores
+# stock_financial_data = stocks.financial_data
+# stock_key_stats = stocks.key_stats
 
 returndict = lambda a: a['aapl']
 
-# for x in stocks[0]:
-#     print(x, stocks[x])
-#     print('#########')
+print('Page1 - Free Cash Flow (unit: M) - (B9, C9, D9, E9):')
+print('NetIncome')
+print(stocks.cash_flow(frequency='a'))
 
-print('Asset profile start')
-print(returndict(stock_asset_profile))
-print('#########')
-# for x in stock_asset_profile:
-#     print(x,stock_asset_profile[x])
-#     print('#########')
+print('Page1 - Revenue (unit: M) - (B12, C12, D12, E12):')
+print('TotalRevenue')
+print(stocks.income_statement(frequency='a'))
 
-print('Esg Scores start')
-print(returndict(stock_esg_scores))
-print('#########')
+# print('Asset profile start')
+# print(returndict(stock_asset_profile))
+# print('#########')
+
+# print('Esg Scores start')
+# print(returndict(stock_esg_scores))
+# print('#########')
+
+# print('Financial data start')
+# print(returndict(stock_financial_data))
+# print('#########')
+
+# print('Key stats start')
+# print(returndict(stock_key_stats))
+# print('#########')
